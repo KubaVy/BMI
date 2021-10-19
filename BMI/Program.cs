c8ebd6d7e1ca47d7d1b821a5c14e4b4de63e0eb3
Jakub Vykydal <Vykydal.Jakub@stud.vassboskovice.cz>
2021-10-19 11:23:25 +0200

    BMI

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -6,7 +6,33 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Výpočet BMI");
+
+            Console.ReadLine();
+
+            double hmotnost;
+            double výška;
+            double BMI;
+
+            Console.WriteLine("Zadej hmotnost");
+            hmotnost = double.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej výšku");
+            výška = double.Parse(Console.ReadLine());
+            BMI = hmotnost / ((výška / 100) * (výška / 100));
+            BMI = Math.Round(BMI, 1);
+            Console.WriteLine($"Vaše BMI je {BMI}");
+            Console.ReadLine();
+            
+
+
+
+
+
+
+
+
+
+        }   
+
     }
 }
